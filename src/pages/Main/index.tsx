d48083c242718d514f3a5e8a2e9a19474d627e44
r3nanp <renanmol87@gmail.com>
2020-11-18 13:25:26 -0300

    feat (main): add component

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,13 +1,36 @@
 /* eslint-disable no-use-before-define */
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-import { Container } from './styles'
+import Header from '../../components/Header'
+
+import { Container, ProductList, Actions } from './styles'
 
 const Main: React.FC = () => {
   return (
-    <Container>
-      <h1>Main</h1>
-    </Container>
+    <>
+      <Header />
+      <Container>
+        <ProductList>
+          <article>
+            <strong>React Native</strong>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel odit
+              recusandae repudiandae suscipit ex debitis officia voluptatum
+              dolorem, laudantium perspiciatis deserunt sit cum corrupti! Facere
+              vel ratione fugit harum dignissimos!
+            </p>
+
+            <Link to="/product">Acessar</Link>
+          </article>
+        </ProductList>
+
+        <Actions>
+          <button>Anterior</button>
+          <button>Próximo</button>
+        </Actions>
+      </Container>
+    </>
   )
 }
 
--- src/pages/Main/styles.ts
+++ src/pages/Main/styles.ts
@@ -1,5 +1,81 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 
-`;
+export const ProductList = styled.div`
+  max-width: 700px;
+  margin: 20px auto 0;
+  padding: 0 20px;
+
+  > article {
+    background: white;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    padding: 20px;
+    margin-bottom: 20px;
+
+    strong {
+      font-size: 30px;
+      font-weight: 700;
+    }
+
+    p {
+      font-size: 16px;
+      color: #999;
+      margin-top: 5px;
+      line-height: 24px;
+    }
+
+    a {
+      height: 42px;
+      border-radius: 5px;
+      border: 2px solid var(--primary);
+      background: none;
+      margin-top: 10px;
+      color: var(--primary);
+      font-weight: 700;
+      font-size: 16px;
+      text-decoration: none;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      transition: all 0.3s;
+
+      &:hover {
+        background: var(--primary);
+        color: #fff;
+      }
+    }
+  }
+`
+
+export const Actions = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+
+  > button {
+    padding: 10px;
+    border-radius: 5px;
+    border: 0;
+    background: var(--primary);
+    color: #fff;
+    font-size: 16px;
+    font-weight: bold;
+    cursor: pointer;
+    transition: 0.3s;
+
+    &:hover {
+      opacity: 0.7;
+    }
+  }
+
+  > button:nth-child(1) {
+    margin-right: 15px;
+  }
+`
