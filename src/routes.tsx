7a5ff12f8ba99d943aba5d75156b3797fdfe6edc
r3nanp <renanmol87@gmail.com>
2020-11-20 09:54:11 -0300

    feat (routes); add params in product component and add another route

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,13 +4,15 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Main from './pages/Main'
 import Product from './pages/Product'
+import CreateProduct from './pages/CreateProduct'
 
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" component={Main} exact />
-        <Route path="/product" component={Product} />
+        <Route path="/product/:id" component={Product} />
+        <Route path="/create/product" component={CreateProduct} />
       </Switch>
     </BrowserRouter>
   )
